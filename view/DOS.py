cbcefca21ceba8bfb95e70ea1be7fd6f8135a120
rbrandolt <brandol.jr@gmail.com>
2026-08-02 08:52:42 -0300

    I've never done PDOS before!

--- view/DOS.py
+++ view/DOS.py
@@ -69,7 +69,7 @@ def run_dftb(mol, mol_name, out_path, interactive_plot, dos_range, plot_PDOS = F
                 shell = data[1]
                 orbital = data[2]
                 dos_file = f'{output.replace('.out','')}.dos.dat'
-                os.system(f'dp_dos {out_path}{output} {out_path}{dos_file}')
+                os.system(f'dp_dos -w {out_path}{output} {out_path}{dos_file}')
                 ene, dos = read_dos_dftb(out_path, dos_file)
                 ene, dos = center_to_fermi(ene, dos, fermi_e, dos_range)
                 ax.plot(ene, dos, label=f'pdos: {atom} {shell_dict[shell]} {orbital}')
