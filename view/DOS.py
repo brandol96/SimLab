f2b828fb3a932dab7631bace3ce754c6f24aca29
rbrandolt <brandol.jr@gmail.com>
2026-09-03 16:51:19 -0300

    improvements to PDOS plot

--- view/DOS.py
+++ view/DOS.py
@@ -28,6 +28,8 @@ def center_to_fermi(energy, dos, fermi_e, dos_range ,eigen = []):
 
 
 def run_dftb(mol, mol_name, out_path, interactive_plot, dos_range, plot_PDOS = False):
+    from matplotlib import colormaps
+
     os.system(f'dp_dos {out_path}band.out {out_path}{mol_name}.dos.dat')
 
     dos_file = f'{mol_name}.dos.dat'
@@ -41,8 +43,8 @@ def run_dftb(mol, mol_name, out_path, interactive_plot, dos_range, plot_PDOS = F
     ene, dos, eigen = center_to_fermi(ene, dos, fermi_e, dos_range, eigen)
 
     # plot stuff
-    ax.plot(ene, dos)
-    ax.plot([0,0],[min(dos),max(dos)],'--')
+    ax.plot(ene, dos, color='black')
+    ax.plot([0,0],[min(dos),max(dos)],'--',color='grey')
     for e in eigen:
         if -dos_range <= e <= dos_range:
             ax.plot([e, e], [0, 0.1 * max(dos)], color='black')
@@ -58,9 +60,14 @@ def run_dftb(mol, mol_name, out_path, interactive_plot, dos_range, plot_PDOS = F
                   '3':'d',
                   '4':'f',}
 
-    if plot_PDOS:
+    group_shells = False
+
+    if plot_PDOS and not group_shells:
         out_files = os.listdir(out_path)
         out_files.sort()
+        base_cmap = colormaps['tab20']
+        discrete_colors = [base_cmap(i) for i in range(20)]
+        c_idx = 0
         for output in out_files:
             if 'PDOS' in output and '.dos.dat' not in output:
                 print(output)
@@ -73,11 +80,50 @@ def run_dftb(mol, mol_name, out_path, interactive_plot, dos_range, plot_PDOS = F
                 os.system(f'dp_dos -w {out_path}{output} {out_path}{dos_file}')
                 ene, dos = read_dos_dftb(out_path, dos_file)
                 ene, dos = center_to_fermi(ene, dos, fermi_e, dos_range)
-                ax.plot(ene, dos, label=f'pdos: {atom} {shell_dict[shell]} {orbital}')
+                ax.plot(ene, dos, color = discrete_colors[c_idx],label=f'pdos: {atom} {shell_dict[shell]} {orbital}')
+                c_idx+=1
+
+    if plot_PDOS and group_shells:
+        pdos_data_dict = {}
+        unique_elements = set()
+        out_files = os.listdir(out_path)
+        out_files.sort()
+
+        for output in out_files:
+            if 'PDOS' in output and '.dos.dat' not in output:
+                print(output)
+                data = output.split('_')
+                data = data[1].split('.')
+                atom = data[0]
+                shell = data[1]
+                #orbital = data[2]
+                dos_file = f'{output.replace('.out','')}.dos.dat'
+                os.system(f'dp_dos -w {out_path}{output} {out_path}{dos_file}')
+                ene, dos = read_dos_dftb(out_path, dos_file)
+                ene, dos = center_to_fermi(ene, dos, fermi_e, dos_range)
+
+                unique_elements.add(atom)
+                group_key = (atom, shell)
+                if group_key in pdos_data_dict:
+                    pdos_data_dict[group_key] = [x + y for x, y in zip(dos, pdos_data_dict[group_key])]
+                else:
+                    pdos_data_dict[group_key] = dos
+
+                element_counters = {elem: 0 for elem in unique_elements}
+
+        for (atom, shell), final_dos in sorted(pdos_data_dict.items()):
+             # Plot the grouped subshell line
+            ax.plot(ene,final_dos,label=f'Atom {atom} ({shell_dict[shell]})')
+
 
     plt.tight_layout()
-    ax.legend()
-    fig.savefig(f'{out_path}DFTB_{mol_name}_DOS.png')
+    plt.xlim(-dos_range,dos_range)
+    ax.legend(loc='upper right')
+
+    if group_shells:
+        fig.savefig(f'{out_path}DFTB_{mol_name}_DOS_shellGroup.png')
+    else:
+        fig.savefig(f'{out_path}DFTB_{mol_name}_DOS.png')
     if interactive_plot:
         plt.show()
     else:
